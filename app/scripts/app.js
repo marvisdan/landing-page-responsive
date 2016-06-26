4901ef7bfb047d3f1f0b936ddda16e93aa3bb9b6
Marvis Dan <marvis.dan@gmail.com>
2016-06-26 22:51:41 +0200

    page responsive

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name starwarsApp
+ * @description
+ * # starwarsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('starwarsApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name starwarsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the starwarsApp
+ */
+angular.module('starwarsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
